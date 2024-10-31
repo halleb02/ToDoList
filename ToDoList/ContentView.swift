975680e45b02187e42d0d702122dd34517a1fe71
Halle Black <hcb0046@auburn.edu>
2024-10-31 08:50:27 -0500

    Initial Commit

---
+++ ToDoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Halle Winters on 10/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Halle Winters on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
